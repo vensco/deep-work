201317fcd278f1927613a07a898d64d29fe5728f
ndzf <ndzf.manifest@gmail.com>
2021-11-29 14:59:38 +0700

    fix: ProjectEntity dates

--- app/Entities/ProjectEntity.php
+++ app/Entities/ProjectEntity.php
@@ -7,6 +7,6 @@ use CodeIgniter\Entity\Entity;
 class ProjectEntity extends Entity
 {
     protected $datamap = [];
-    protected $dates   = ['created_at', 'updated_at'];
+    protected $dates   = ["created_at", "updated_at", "dateline"];
     protected $casts   = [];
 }
