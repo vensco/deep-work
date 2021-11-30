c7f22488b1085c0e798b833d7cdf41ac02173847
ndzf <ndzf.manifest@gmail.com>
2021-11-30 18:19:25 +0700

    fix: enable timestamps

--- app/Models/ProjectModel.php
+++ app/Models/ProjectModel.php
@@ -17,7 +17,7 @@ class ProjectModel extends Model
     protected $allowedFields    = ["name", "description", "dateline"];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
